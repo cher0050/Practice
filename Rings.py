c6613fb4c8e3453ae7ccc11dee66294fe384c69d
cher0050 <chmadhubabu1@gmail.com>
2021-12-19 12:06:51 +0530

    Update Rings.py

--- Rings.py
+++ Rings.py
@@ -4,7 +4,9 @@ res=0
 s=[set() for i in range(10)]
 
 for i in range(0,len(rings),2):
+	if s[int(rings[i+1])]=={'X'}: continue
 	s[int(rings[i+1])].add(rings[i])
 	if len(s[int(rings[i+1])])==3:
+		s[int(rings[i+1])]={'X'}
 		res+=1
 print(res)
